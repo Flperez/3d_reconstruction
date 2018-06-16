817a4abfd944e9eba60367db4f7de1848347fd67
f <felipeperez2710@gmail.com>
2018-06-16 14:28:16 +0200

    franja hecho y limitado epipolar

--- MyAlgorithm.py
+++ MyAlgorithm.py
@@ -124,9 +124,10 @@ class MyAlgorithm():
         return np.concatenate((point3ds[:,:3]-posCam*np.ones(pointInOpts.shape), np.ones((pointInOpts.shape[0], 1))), 1)
 
 
-    def getPointsfromline(self,vectors,Origin,M=150):
+    def getPointsfromline(self,vectors,Origin):
         lst = [[] for N in range(vectors.shape[0])]
-        alfas = np.linspace(1,M,M)
+        # alfas = np.linspace(10,20,M)
+        alfas = [10,100,10000]
         for idx,Vxyz in enumerate(vectors):
             for alfa in alfas:
                 xyz = Origin + Vxyz[:3]*alfa
@@ -145,6 +146,51 @@ class MyAlgorithm():
                 projected_lst_grafic[N].append(np.array([projected_grafic[1],projected_grafic[0],1]))
 
         return projected_lst_grafic
+
+
+    def getLineEpipolar(self,lstPoints3d,cam):
+        projected_lst_grafic = []
+        m=0
+        n=0
+        for idx, xyz in enumerate(lstPoints3d):
+            if cam == "right":
+                projected = self.camRightP.project(xyz)
+                projected_grafic = np.floor(self.camRightP.opticalToGrafic(projected)).astype(np.int)
+            if cam == "left":
+                projected = self.camLeftP.project(xyz)
+                projected_grafic = np.floor(self.camLeftP.opticalToGrafic(projected)).astype(np.int)
+            projected_lst_grafic.append(np.array([projected_grafic[1], projected_grafic[0], 1]))
+
+        #Get m y n from y = mx+n
+        begin = projected_lst_grafic[0][1]
+        end = projected_lst_grafic[2][1]
+        m = (projected_lst_grafic[0][0]-projected_lst_grafic[1][0]) / (projected_lst_grafic[0][1]-projected_lst_grafic[1][1])
+        n = projected_lst_grafic[0][0]-m*projected_lst_grafic[0][1]
+        return m,n,begin,end
+
+    def getPointsfromEpipolar(self,m,n,begin,end,size=5):
+        horizontal = np.arange(begin, end+1).reshape(-1, 1)
+        inc = int(size/2)
+        n_inc = np.arange(-inc + n, n + inc + 1)
+        if m == 0:
+            # Par estereo canonico
+            vertical = np.asarray([np.repeat(n,horizontal.shape[0]) for n in n_inc],dtype=int).reshape(-1,1)
+            horizontal_inc = np.repeat(horizontal,n_inc.size,axis=1).T.reshape(-1,1)
+
+            return np.concatenate((vertical,horizontal_inc),axis=1)
+        else:
+            vertical = np.asarray([np.asarray([m*x+n for x in horizontal],dtype=int) for n in n_inc],dtype=int).reshape(-1,1)
+            horizontal_inc = np.repeat(horizontal,n_inc.size,axis=1).T.reshape(-1,1)
+            # Limit point coordinate
+            idx = np.where(vertical<self.height)[0].tolist()
+            if len(idx)>0:
+                vertical_limit = vertical[idx,:]
+                horizontal_limit = horizontal_inc[idx,:]
+            else:
+                vertical_limit = vertical
+                horizontal_limit = horizontal_inc
+
+            return np.concatenate((vertical_limit,horizontal_limit),axis=1)
 
 
 
@@ -171,10 +217,9 @@ class MyAlgorithm():
             if len(lstPoints.shape)==1:
                 out[lstPoints[0], lstPoints[1]] = color
             else:
-                for uv in lstPoints:
-                    out[uv[0], uv[1]] = color
+                out[lstPoints[:,0], lstPoints[:,1],:] = color
         else:
-            out[lstPoints[idx,0],lstPoints[idx,1]]=color
+            out[lstPoints[idx,0],lstPoints[idx,1]]= color
 
 
         return out
@@ -282,25 +327,24 @@ class MyAlgorithm():
         point = point.astype(np.int)
         return image[point[0],point[1],:].tolist()
 
-    def matchingPoint(self, keyPointL, lst2matchingR, imageR, imageL, edgesR,outR, outL, size=(11, 11),save=False):
-        hsvR = cv2.cvtColor(imageR, cv2.COLOR_RGB2HSV)
-        hsvL = cv2.cvtColor(imageL, cv2.COLOR_RGB2HSV)
-        alfa = 0.75
-        beta = 0.25
+    def matchingPoint(self, keyPointL, lst2matchingR, edgesR,outR, outL, size=(7, 7)):
+
+        alfa = 0.6
+        beta = 0.4
         incu = int(size[0] / 2)
         incv = int(size[1] / 2)
 
-        patchL = hsvL[keyPointL[0] - incu:keyPointL[0] + incu + 1, keyPointL[1] - incv:keyPointL[1] + incv + 1, :]
+        patchL = self.hsvL[keyPointL[0] - incu:keyPointL[0] + incu + 1, keyPointL[1] - incv:keyPointL[1] + incv + 1, :]
         maximum = 0.5
-        best = np.zeros([3, ])
+        best = np.zeros([3,])
+
+
 
         # eliminamos repetidos
-        lst2matchingR_unique = np.array(list(set(tuple(p) for p in np.asarray(lst2matchingR))))
-        for jdx, uvR in enumerate(lst2matchingR_unique):
-            # print "uvR,jdx",uvR,jdx
+        for jdx, uvR in enumerate(lst2matchingR):
             # Solo buscamos el matching si es un pixel de contorno
             if edgesR[uvR[0], uvR[1]] == 255:
-                patchR = hsvR[uvR[0] - incu:uvR[0] + incu + 1, uvR[1] - incv:uvR[1] + incv + 1, :]
+                patchR = self.hsvR[uvR[0] - incu:uvR[0] + incu + 1, uvR[1] - incv:uvR[1] + incv + 1, :]
                 corr = alfa * MyAlgorithm.correlation_coefficient(self, patchL[:, :, 0], patchR[:, :, 0]) \
                        + beta * MyAlgorithm.correlation_coefficient(self, patchR[:, :, 1], patchR[:, :, 1])
                 if corr > maximum:
@@ -330,11 +374,19 @@ class MyAlgorithm():
         # OBTENCIÓN DE IMÁGENES
         imageLeft = self.sensor.getImageLeft()
         imageRight = self.sensor.getImageRight()
+        self.width = imageRight.shape[1]
+        self.height = imageRight.shape[0]
+        self.time = time.clock()
+        self.outEpipol = np.zeros((self.height,self.width*2,3),dtype=np.uint8)
 
         self.OR = self.camRightP.getCameraPosition()
         self.OL = self.camLeftP.getCameraPosition()
+        self.hsvR = cv2.cvtColor(imageRight, cv2.COLOR_RGB2HSV)
+        self.hsvL = cv2.cvtColor(imageLeft, cv2.COLOR_RGB2HSV)
+
 
         os.system("killall gzserver")
+
         save = False
         write = True
         visor = True
@@ -356,16 +408,25 @@ class MyAlgorithm():
 
         # Get M points from N vectors: NxM
         print "Calculating list of 3D points "
-        lstPoints3d_L = MyAlgorithm.getPointsfromline(self,vectorL,self.camLeftP.getCameraPosition(),M=150)
+        lstPoints3d_L = MyAlgorithm.getPointsfromline(self,vectorL,self.camLeftP.getCameraPosition())
         print "Dimension",len(lstPoints3d_L),"x",len(lstPoints3d_L[0])
 
         # Get NxM projected points from NxM
 
         for idx,Points3d_L in enumerate(lstPoints3d_L):
-            if idx%100==0:
-                print idx,'/',len(lstPoints3d_L)
+            if idx%25==0 and idx>0:
+                print idx,'/',len(lstPoints3d_L),"T: ", time.clock() - self.time
+                # self.outEpipol[:,:self.width,:]=self.outL_line
+                # self.outEpipol[:,self.width:,:]=self.outR_line
+                # cv2.imwrite("result/imagenes_epipolar/LR/imag_epipol" + str(idx) + ".png",  self.outEpipol)
 
-            projected_R = MyAlgorithm.getProjectedPoints(self,Points3d_L,cam="right")
+                # cv2.imwrite("result/imagenes_epipolar/R/imagR_epipol"+str(idx)+".png",self.outR_line)
+                # cv2.imwrite("result/imagenes_epipolar/L/imagL_epipol"+str(idx)+".png",self.outL_line)
+
+            # print "K", keypointsL[idx,:]
+
+            m, n, begin, end = MyAlgorithm.getLineEpipolar(self,Points3d_L,cam="right")
+            projected_R = MyAlgorithm.getPointsfromEpipolar(self,m,n,begin,end)
 
             # Eliminamos los repetidos
             projected_R_unique =  np.array(list(set(tuple(p) for p in np.asarray(projected_R))))
@@ -374,8 +435,7 @@ class MyAlgorithm():
             outR_line = MyAlgorithm.drawPoint(self,outR,projected_R_unique,None,(255,255,0))
             self.setRightImageFiltered(outR_line)
 
-            pointR,outR,outL = MyAlgorithm.matchingPoint(self,keypointsL[idx,:], projected_R_unique,imageRight,
-                                                                imageLeft,edgesR,outR,outL,save=save)
+            pointR,outR,outL = MyAlgorithm.matchingPoint(self,keypointsL[idx,:], projected_R_unique,edgesR,outR,outL)
 
 
 
@@ -384,13 +444,13 @@ class MyAlgorithm():
                 point3DColor = MyAlgorithm.triangulate(self,pointR,imageRight,vectorL,idx)
 
                 if write:
-                    MyAlgorithm.writeTxt(self,"ptsColor_unoporuno.txt",point3DColor,mode="unico")
+                    MyAlgorithm.writeTxt(self,"result/ptsColor_franja5_2.txt",point3DColor,mode="unico")
 
                 if visor:
                     self.sensor.drawPoint(np.array([point3DColor[0], point3DColor[1], point3DColor[2]]),
                                       (point3DColor[3], point3DColor[4], point3DColor[5]))
 
-
+        print "Tiempo Total: ",time.clock()-self.time
         if self.done:
             return
 
